36432792aadae7b2eb5beea7ef43d9b155689e47
Aviral Mittal <aviralmittal0012@gmail.com>
2026-02-28 20:31:23 +0530

    First Commit - RAGAS

---
+++ text_to_sql.sql
@@ -0,0 +1,10 @@
+create database text_to_sql;
+use text_to_sql;
+select * from customers; --  Customer Index
+select * from sales_order; -- Customer Name Index
+
+#what is the total 'Line Total' for Geiss Company?
+select s.`Customer Name Index`, SUM(s.`Line Total`) from customers c, sales_order s
+where c. `Customer Index` = s. `Customer Name Index`
+and c. `Customer Names` = 'Geiss Company'
+group by s. `Customer Name Index`;
